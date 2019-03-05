a880161f5f1c5c9c057d459172b7655d9f52e206
cplcurry3035 <953058313@qq.com>
2019-03-05 18:50:36 +0800

    kd分支版本1

--- back_stage_management/src/api/routers/addlist.js
+++ back_stage_management/src/api/routers/addlist.js
@@ -1,10 +1,58 @@
-/* 
-* @Author: Marte
-* @Date:   2019-02-27 20:29:58
-* @Last Modified by:   Marte
-* @Last Modified time: 2019-02-27 20:30:14
-*/
+const Router = require('koa-router');
 
-$(document).ready(function(){
+const db = require('../db');
+
+// 创建路由
+var router = new Router();
+
+const ObjectId = require('mongodb').ObjectID;
+
+
+/**
+ * ctx
+ */
+router.post('/',async (ctx,next)=>{
+
+
+
+
+let {_id,title,pricing, sellingPrice,
+repertory,img,uploadtime,salenum,classify}=ctx.request.body;
+console.log('qqq',_id,title,pricing, sellingPrice,
+repertory,img,uploadtime,salenum,classify)
+
+let  res=await db.find('goodslist',{"_id":new ObjectId(_id)});
+    console.log(res.length,img);
+// 更新
     
+    
+    if(res.length>0){
+        await db.update('goodslist',{"_id":new ObjectId(_id)},{$set:{'title':title,'pricing':pricing,'sellingPrice':sellingPrice,
+         'sellingPrice':repertory,'img':img,'uploadtime':uploadtime,'salenum':salenum,'classify':classify}});
+        // console.log(888,img);
+        ctx.body ={msg:'更新成功'};
+
+
+
+
+
+    }
+    else if(res.length==0){
+         await db.insert('goodslist',{title,pricing, sellingPrice,
+         repertory,img,uploadtime,salenum,classify});
+         ctx.body ={msg:'插入成功'};
+
+    }
+    
+
+
+
+    
+
 });
+
+
+
+module.exports = router;
+
+
